f67e6ef2349eb26b1a4d09d6369eddd63dc4daac
FARHAN AHMAD <117432911+789farhan@users.noreply.github.com>
2024-07-30 16:09:46 +0500

    braintree-payment

---
+++ src/app/api/getclientToken/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server"
+
+export const GET=async(req:any,res:any)=>{
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-
+import Requests from "@/components/Requests"
 export default function Home() {
   return (
     <>
+    <Requests/>
     </>
   );
 }
